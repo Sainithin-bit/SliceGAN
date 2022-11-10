a96d67345948acf19e81831fea077b534d0e4779
SAINITHIN.ARTHAM <sai.artham.19cse@bmu.edu.in>
2022-11-10 14:41:40 +0530

    qwer

--- run_slicegan.py
+++ run_slicegan.py
@@ -25,12 +25,12 @@ Project_path = util.mkdr(Project_name, Project_dir, Training)
 # n-phase materials must be segmented)
 image_type = 'nphase'
 # img_channels should be number of phases for nphase, 3 for colour, or 1 for grayscale
-img_channels = 256
+img_channels = 3
 # define data type (for colour/grayscale images, must be 'colour' / '
 # greyscale. nphase can be, 'tif2D', 'png', 'jpg', tif3D, 'array')
-data_type = 'jpg'
+data_type = ''
 # Path to your data. One string for isotrpic, 3 for anisotropic
-data_path = ['Examples/17AR-1_1_.jpg']
+data_path = ['Examples/17AR-1_1_.tif']
 
 ## Network Architectures
 # Training image size, no. channels and scale factor vs raw data
